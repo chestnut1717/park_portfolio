15d1760408d6919c224d5ffd4f218f618e0b7a22
Park <psh56750855@gmail.com>
2022-02-07 23:32:08 +0900

    CSS(~testimonial) / JS(typing effect)

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+var typingBool = false; 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = document.querySelector(".text__typing").childElementCount;
+
+
+var findText = document.getElementsByClassName("text__typing");
+var typingTextList = findText[0].innerText.split("\n");
+
+// var typingText = typingTextList[liIndex];
+
+if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+    typingBool=true; 
+    var tyInt = setInterval(typing,100); // 반복동작 
+} 
+     
+function typing(){
+  var typingText = typingTextList[liIndex];
+  var typedText = document.querySelector(".typing").innerText;
+  console.log(typingText);
+  if(typingIdx<typingText.length){ // 타이핑될 텍스트 길이만큼 반복 
+    typedText = typedText + typingText[typingIdx]; // 한글자씩 이어준다.
+    document.querySelector('.typing').innerText = typedText;
+    typingIdx++; 
+
+   } else{ //한문장이끝나면
+     //다음문장으로.. 마지막문장이면 다시 첫번째 문장으로 
+     if(liIndex>=liLength-1){
+       liIndex=0;
+     }else{
+       liIndex++; 
+     }
+     
+     //다음문장을 타이핑하기위한 셋팅
+        typingIdx=0;
+        typingBool = false; 
+       
+     //다음문장 타이핑전 1초 쉰다
+         clearInterval(tyInt);
+         setTimeout(function(){
+            document.querySelector('.typing').innerText = "";
+            tyInt = setInterval(typing,100);
+         },1000);
+    } 
+}  
+
+
+
 // Skills menu
 
 const skillMenu = document.querySelector(".skill__menu");
