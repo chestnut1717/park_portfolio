d262f3f697b85082ac3530510a004398b447ff14
Park <psh56750855@gmail.com>
2022-02-09 23:53:21 +0900

    CSS navbar, home color / blink effect

--- main.js
+++ main.js
@@ -2,32 +2,35 @@ var typingBool = false;
 var typingIdx=0; 
 var liIndex = 0;
 var liLength = document.querySelector(".text__typing").childElementCount;
-
+// Cursor Active Deactive
+var cursor = document.querySelector(".cursor");
 
 var findText = document.getElementsByClassName("text__typing");
 var typingTextList = findText[0].innerText.split("\n");
 
-// var typingText = typingTextList[liIndex];
 
 if(typingBool==false){ // 타이핑이 진행되지 않았다면 
     typingBool=true; 
-    var tyInt = setInterval(typing,100); // 반복동작 
-} 
+    var tyInt = setInterval(typing,110); // 반복동작 
+}
      
 function typing(){
   var typingText = typingTextList[liIndex];
   var typedText = document.querySelector(".typing").innerText;
 
+
   if (typingIdx<typingText.length){ // 타이핑될 텍스트 길이만큼 반복 
     typedText = typedText + typingText[typingIdx]; // 한글자씩 이어준다.
     document.querySelector('.typing').innerText = typedText;
     typingIdx++; 
+    cursor.classList.remove("blink");
    } else { //한문장이끝나면
      //다음문장으로.. 마지막문장이면 다시 첫번째 문장으로 
-     if (liIndex>=liLength-1){
+     if (liIndex>=liLength){
        liIndex=0;
      } else {
        liIndex++; 
+       console.log(liIndex);
      }
      
      //다음문장을 타이핑하기위한 셋팅
@@ -36,10 +39,11 @@ function typing(){
        
      //다음문장 타이핑전 1초 쉰다
          clearInterval(tyInt);
+         cursor.classList.add("blink");
          setTimeout(function(){
             document.querySelector('.typing').innerText = "";
-            tyInt = setInterval(typing,100);
-         },1000);
+            tyInt = setInterval(typing,110);
+         },900);
     } 
 }  
 
