81cce3531f9d6296a391bc2e26b27069e84fd35a
Park <psh56750855@gmail.com>
2022-02-12 18:09:21 +0900

    JS (scrolling effect, navbar effect)

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+'use strict';
+
+// Transparent Navbar
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({'behavior' : 'smooth'});
+}
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// button to scroll
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+
+// Trahsparent When Scrolling
+const io = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });                            
+});
+
+document.querySelectorAll('.section__container').forEach((wrapper) => io.observe(wrapper));
+
 var typingBool = false; 
 var typingIdx=0; 
 var liIndex = 0;
