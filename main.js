bbdb9e8cc5d6cb8654a5c68368dd1df1fee0340c
Park <psh56750855@gmail.com>
2022-02-14 00:24:05 +0900

    JS (navbar toggle btn)

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ document.addEventListener('scroll', () => {
 
 // button to scroll
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (e) => {
   const target = e.target;
   const link = target.dataset.link;
@@ -25,6 +24,13 @@ navbarMenu.addEventListener('click', (e) => {
   scrollIntoView(link);
 });
 
+
+// Navbar toggle button open menu
+const navbarToggleButton = document.querySelector('.navbar__toggle-btn');
+
+navbarToggleButton.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
 
 // Trahsparent When Scrolling
 const io = new IntersectionObserver((entries, observer) => {
@@ -175,3 +181,33 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({'behavior' : 'smooth'});
 }
+
+
+// click project button
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  
+  // Remove Selection from the previous item and select new one
+  const active = document.querySelector('.work__categories > .selected');
+  active.classList.remove('selected');
+  
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() =>{
+    projects.forEach((project) => {
+      if(filter === '*' || filter == project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+
+      }
+    })
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
