ef2abf7108c4b42830d89c4a79cc665db5a87716
Park <psh56750855@gmail.com>
2022-02-15 19:32:02 +0900

    Design (skill & title)

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function typing(){
 
 const skillMenu = document.querySelector(".skill__menu");
 const skillMenuAll = document.querySelectorAll(".skill__menu > .skill__menu__item");
-const skills = document.querySelectorAll(".skill__graph > .skill");
+const skills = document.querySelectorAll(".skill__graph  .skill");
 
 // skills to be invisible when page is first opened
 skills.forEach((skill) => {
